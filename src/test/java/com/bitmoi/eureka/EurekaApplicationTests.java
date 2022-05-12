888b3252fd65aa501cbf6ff717a3735978f5a34f
Jungwook Kim <jw0905@naver.com>
2022-05-12 19:59:53 +0900

    eureka

---
+++ src/test/java/com/bitmoi/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitmoi.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
